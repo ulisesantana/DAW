d0ac159542f7815fdd990cef4285cacd927e882c
Ulises Santana <ulisesantana@gmail.com>
2016-09-21 14:36:05 +0100

    Día 20/10/2016

---
+++ DEW/tema1/ejercicio1/js/codigo.js
@@ -0,0 +1,13 @@
+//Función para generar el alert cuando se llame
+function ver(){
+  alert('Hola Mundo!');
+}
+//Alert que se lanza al cargarse el archivo codigo.js
+const saludoInvariable = 'Soy el primer script y es fácil usar \'comillas simples\' y "comillas dobles"';
+alert(saludoInvariable);
+
+//Lanzamos un prompt para preguntar la edad
+function age(){
+  var age = prompt('Dime tu edad');
+  alert('Tienes '+age+' años.');
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-DAW
+Ejercicios y algunos apuntes de las asignaturas de 2º de Desarrollo de Aplicaciones Web.
