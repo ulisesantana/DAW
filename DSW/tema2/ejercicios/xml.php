f76043d17b7675bb6685ef166777507e1af74737
Ulises Santana <ulisesantana@gmail.com>
2016-10-21 10:22:59 +0100

    Día 20/10/2016

---
+++ DSW/tema2/ejercicios/xml.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Testing XML & PHP</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+      <style media="screen">
+          @import url('https://fonts.googleapis.com/css?family=Montserrat');
+          body {
+              background-color: #eee;
+              font-family: "Montserrat", sans-serif;
+          }
+
+          #profile {
+              background-color: #333;
+              color: #fff;
+              height: 100vh;
+              width: 25vw;
+              position: fixed;
+          }
+
+          #profile img {
+              max-width: 150px;
+              margin: 20px auto;
+          }
+
+          #profile h3 {
+              text-align: center;
+              padding: 15px;
+          }
+
+          #profile li.active{
+            background-color: #eee;
+          }
+
+          #main {
+              width: 73vw;
+              padding: 2% 3%;
+              margin-left: 24vw;
+          }
+
+          .space{padding-top:5%;}
+      </style>
+</head>
+
+<body>
+
+    <div id="profile">
+        <img src="https://avatars3.githubusercontent.com/u/17091490?v=3&s=466" alt="" class="img-responsive img-circle">
+        <h3 class="text-center">Ulises Santana Suárez <br><small>2º DAW</small></h3>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav">
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+    </div> <!-- /profile -->
+
+    <div id="main">
+      <?php
+        libxml_use_internal_errors(true);
+        $xml = simplexml_load_file('notas.xml');
+        if ($xml === false) {
+          echo 'Error en el archivo XML';
+        } else {
+          echo '<table class="table">';
+          echo '<th colspan="2" class="text-center lead">'.$xml->asignatura.'</th>';
+          foreach ($xml->alumno as $alumno) {
+            echo '<tr>';
+            echo '<td>'.$alumno->nombre.'</td>';
+            echo '<td>'.$alumno->nota.'</td>';
+            echo '</tr>';
+          }
+          echo '</table>';
+        }
+
+       ?>
+
+    </div> <!-- /main -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+
+</html>
