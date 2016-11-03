7d50b94475cd9b634daba7566c42f526be0f8dee
Ulises Santana <ulisesantana@gmail.com>
2016-11-03 11:21:37 +0000

    Añadido municipio como #town

--- J4F/testingSchoolApp/tasks.js
+++ J4F/testingSchoolApp/tasks.js
@@ -1,33 +1,7 @@
-function render(id, code){
-  let html = (code!=null) ? document.getElementById(id).innerHTML = code
-    : document.getElementById(id).innerHTML = '<br>';
-  return html;
-}
-
-//Hacer lo que falta
-/*TODO
-  - Agrupar tareas por prioridad y ordenarlas por prioridad y tiempo de ejecución
-  - Primero lo más prioritario que ocupe menos tiempo
-  - Debería añadirse en algún momento el tener en cuenta el deadline.
-  - En un futuro el código podría calcular el tiempo hasta el deadline y
-    preveer que no te da tiempo hacerlo todo en función a las horas de
-    producción que tengas asignadas diariamente y si tiene en cuenta si
-    trabajas los fines de semana o no
-*/
-
-function printTasks(id,tasks){
-  let html ='';
-
-  for (task of tasks) {
-    html += '<li>'+task.name+'</li>';
-  }
-
-  return render(id,html);
-}
-
-function orderTasks(tasks) {
-  //Ordenar las tareas primero por tiempo y luego por prioridad
-  return tasks;
+function render(id, code) {
+    let html = (code != null) ? document.getElementById(id).innerHTML = code :
+        document.getElementById(id).innerHTML = '<br>';
+    return html;
 }
 
 class Task {
@@ -39,15 +13,85 @@ class Task {
 
 }
 
-var tasks =[
-  new Task("Tarea nº1: Baja - 2 min", 2, 4 ),
-  new Task("Tarea nº2: Media - 15 min", 15, 3 ),
-  new Task("Tarea nº3: Alta - 30 min", 30, 2 ),
-  new Task("Tarea nº4: Urgente - 60 min", 60, 1),
-  new Task("Tarea nº5: Media - 10 min", 10, 3 )
+//Hacer lo que falta
+/*TODO
+  - Hacer más modulable el ordenamiento por prioridad
+  - Debería añadirse en algún momento el tener en cuenta el deadline.
+  - En un futuro el código podría calcular el tiempo hasta el deadline y
+    preveer que no te da tiempo hacerlo todo en función a las horas de
+    producción que tengas asignadas diariamente y si tiene en cuenta si
+    trabajas los fines de semana o no
+*/
+
+function printTasks(id, tasks) {
+    let html = '';
+
+    for (task of tasks) {
+        html += '<li>' + task.name + '</li>';
+    }
+
+    return render(id, html);
+}
+
+function sortByTime(tasks) {
+    let len = tasks.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (tasks[i].neededTime < tasks[j].neededTime) {
+                let temp = tasks[i];
+                tasks[i] = tasks[j];
+                tasks[j] = temp;
+            }
+        }
+    }
+
+    return tasks;
+}
+
+function checkUrgentPriority(tasks) {
+    return tasks.priority == 1;
+}
+
+function checkHighPriority(tasks) {
+    return tasks.priority == 2;
+}
+
+function checkMediumPriority(tasks) {
+    return tasks.priority == 3;
+}
+
+function checkLowPriority(tasks) {
+    return tasks.priority == 4;
+}
+
+function orderTasks(tasks) {
+    let tempTasks = [
+      tasks.filter(checkUrgentPriority),
+      tasks.filter(checkHighPriority),
+      tasks.filter(checkMediumPriority),
+      tasks.filter(checkLowPriority)
+    ];
+
+    for (task of tempTasks) {
+      task = sortByTime(task);
+    }
+    tasks = tempTasks[0].concat(tempTasks[1],tempTasks[2],tempTasks[3])
+
+    return tasks;
+}
+
+
+var tasks = [
+    new Task("Tarea nº1: Baja - 2 min", 2, 4),
+    new Task("Tarea nº2: Media - 15 min", 15, 3),
+    new Task("Tarea nº3: Alta - 30 min", 30, 2),
+    new Task("Tarea nº4: Urgente - 60 min", 60, 1),
+    new Task("Tarea nº5: Alta - 20 min", 20, 2),
+    new Task("Tarea nº6: Media - 35 min", 35, 3),
+    new Task("Tarea nº7: Alta - 10 min", 10, 2)
 ];
 
-printTasks('disordered',tasks);
+printTasks('disordered', tasks);
 
 var orderedTasks = orderTasks(tasks);
-printTasks('ordered',orderedTasks);
+printTasks('ordered', orderedTasks);
